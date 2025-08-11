e380140bbc41695e48824a141402c757b2849c52
OverbearingPearl <OverbearingPearl@outlook.com>
2025-08-11 23:10:34 +0800

    Configure Emacs package management with Tsinghua mirrors
    Added package initialization and set up archive repositories:
    - Require 'package module and initialize
    - Configured four package archives using Tsinghua University mirrors:
      * gnu
      * nongnu
      * melpa-stable
      * melpa
    
    The Tsinghua mirrors provide faster package downloads in China.
    
    Signed-off-by: OverbearingPearl <OverbearingPearl@outlook.com>

--- init.el
+++ init.el
@@ -1,2 +1,10 @@
 (dolist (dir '("common" "modules" "private"))
   (add-to-list 'load-path (expand-file-name dir user-emacs-directory)))
+
+(require 'package)
+(setq package-archives
+      '(("gnu"   . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+        ("nongnu" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
+        ("melpa-stable" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/stable-melpa/")
+        ("melpa" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize)
