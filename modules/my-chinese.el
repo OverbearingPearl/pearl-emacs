ff62584fba15ff5c4583db71e4dd7debbe3cb373
OverbearingPearl <OverbearingPearl@outlook.com>
2025-09-17 23:28:09 +0800

    Add Chinese input support with angle bracket keybindings
    - Map M-《 to M-< and M-》 to M-> for Chinese keyboard layouts
    - Add new my-chinese module to features template
    - Provide module for easy integration

---
+++ modules/my-chinese.el
@@ -0,0 +1,6 @@
+(define-key global-map (kbd "M-《")
+            (lookup-key global-map (kbd "M-<")))
+(define-key global-map (kbd "M-》")
+            (lookup-key global-map (kbd "M->")))
+
+(provide 'my-chinese)
