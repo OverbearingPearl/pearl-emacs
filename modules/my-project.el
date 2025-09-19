e07dd4e7aed4bea5a4ef7eae467c139556d45635
OverbearingPearl <OverbearingPearl@outlook.com>
2025-09-19 17:33:12 +0800

    Add projectile git grep support
    Set `projectile-use-git-grep` to t for faster git-based searching
    
    Signed-off-by: OverbearingPearl <OverbearingPearl@outlook.com>

--- modules/my-project.el
+++ modules/my-project.el
@@ -1,5 +1,7 @@
 (use-package projectile
   :hook (after-init . projectile-mode)
-  :bind ("C-c p" . 'projectile-commander))
+  :bind ("C-c p" . 'projectile-commander)
+  :config
+  (setq projectile-use-git-grep t))
 
 (provide 'my-project)
