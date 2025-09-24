bcd6c9dd1397d51786530ec70b1f5c248f27de22
OverbearingPearl <OverbearingPearl@outlook.com>
2025-09-24 12:34:32 +0800

    Add embark and embark-consult packages
    - Bind embark-act to C-., embark-dwim to C-;, embark-bindings to C-h B
    - Set prefix-help-command to embark-prefix-help-command
    - Configure embark collect buffers to hide mode line
    - Enable consult-preview-at-point-mode in embark-collect-mode
    
    Signed-off-by: OverbearingPearl <OverbearingPearl@outlook.com>

--- modules/my-completion.el
+++ modules/my-completion.el
@@ -15,6 +15,22 @@
         completion-category-defaults nil
         completion-category-overrides '((file (styles partial-completion)))))
 
+(use-package embark
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+  :init
+  (setq prefix-help-command #'embark-prefix-help-command)
+  :config
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+(use-package embark-consult
+  :hook (embark-collect-mode . consult-preview-at-point-mode))
+
 (use-package consult
   :bind (("C-s" . consult-line)
          ("C-c M-x" . consult-mode-command)
