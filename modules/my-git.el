41fb1a0b7c5a98bb01ed3006d66e1a20a261ca24
OverbearingPearl <OverbearingPearl@outlook.com>
2025-11-07 23:14:24 +0800

    Enable forge for interacting with Git forge services
    - Configure forge to use a local SQLite database file
    
    Signed-off-by: OverbearingPearl <OverbearingPearl@outlook.com>

--- modules/my-git.el
+++ modules/my-git.el
@@ -5,4 +5,9 @@
        (executable "git" :error-msg "git not found"))
   :config (magit-auto-revert-mode 1))
 
+(use-package forge
+  :after magit
+  :config
+  (setq forge-database-file (expand-file-name "forge-db.sqlite" user-emacs-directory)))
+
 (provide 'my-git)
